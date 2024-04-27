79f1a9a642f05fcf3eb1eb349f35bf0756280554
RAUSHAN KUMAR <160950026+Raushan2709@users.noreply.github.com>
2024-04-27 19:06:00 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,14 @@
+//TYPE CASTING
+#include<stdio.h>
+int main()
+{
+    
+    for(int i=65;i<=90;i++){
+        printf("%d -->",i);
+        char ch=(char)i;
+        printf(" %c \n",ch);
+    }
+   
+    return 0;
+
+}
